2f8754833bcbc1e35eb8afe29027e395cd945880
Vitor <victoranjosbrilhador@gmail.com>
2023-05-30 19:26:16 -0300

    Trabalho completo 30-05

--- service/user.js
+++ service/user.js
@@ -1,15 +1,17 @@
 const prisma = require("../db/prisma")
+const bcrypt = require ("bcrypt")
 
 const getAllUsuario = () => {
     return prisma.user.findMany()
 }
 
 const createUsuario = (nome, email, senha) => {
+    const senhaCriptografada = bcrypt.hashSync(senha, 10)
     return prisma.user.create({
         data:{
             nome,
             email,
-            senha
+            senha:senhaCriptografada
         }
     })
 }
